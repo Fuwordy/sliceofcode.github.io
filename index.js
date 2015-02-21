daae258a029761a6d7aabcb00b301e321e3764ac
sliceofcode <cheesy.fried.bacon@gmail.com>
2015-02-21 10:42:56 -0800

    Website redesign

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document.body).ready(function() {
+	var name = "sliceofcode.";
+	var q = $.map(name.split(""), function(letter) {
+		return $("<span>" + letter + "</span>");
+	});
+
+	var dest = $("#slice");
+
+	var c = 0;
+	var i = setInterval(function() {
+		q[c].appendTo(dest).show();
+		c += 1;
+		if (c >= q.length) clearInterval(i);
+	}, 100);
+
+	var u = setTimeout(function() {
+		$("#slice>span").addClass("curly-underline");
+	}, 100 * name.length + 1000)
+});
